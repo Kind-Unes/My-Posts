a600ad2646f0410981ee75bf7bcd15017513b0e8
Kind.Unes <my_hellalet@esi.dz>
2024-02-08 10:13:55 +0100

    added the second Post

--- Flutter typethis Package/Code Example.dart
+++ Flutter typethis Package/Code Example.dart
@@ -1,20 +1,8 @@
-// Code Example:
-final TypeThisWidget = TypeThis(
-  // The text which will be animated
-  string: 'Hi there! HOW ARE YOU DOING,',
-  // speeed in milliseconds
-  speed: 100,
-  // Text style for the string
-  style: const TextStyle(fontSize:20)
-);
-
-// controllers
-// Used to reset the typing animation
-typeThisWidget.controller.rest();
-
-// Used to freeze the typing animation
-typeThisWidget.controller.freeze();
-
-// Used to unfreeze the typing animation
-typeThisWidget.controller.unfreeze();
-
+final LiquidPullToRefreshWidget = LiquidPullToRefresh(
+  // key if you want to add 
+  key : _refreshIndicatorKey,
+  // refresh callback
+  onRefresh:(){},
+  // your widget tree
+  child : MyWidgetTree(),
+)
